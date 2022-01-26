3f2f868311a2229bb59ef34feea5ba0b1ddbec79
Kuriko <kurikithune@gmail.com>
2022-01-26 14:39:21 +0900

    fix callback url

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ passport.deserializeUser(function (obj, done) { done(null, obj); });
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
+  callbackURL: process.env.HEROKU_URL ? process.env.HEROKU_URL + 'auth/github/callback' : 'http://localhost:8000/auth/github/callback'
 },
   function (accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
